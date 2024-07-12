1742646cd5afc01319db36fb65c4782dee3d3671
agungriyadi19 <iaagungriyadi@gmail.com>
2024-07-13 06:30:08 +0700

    update type data amount in order

--- warmindo-api/db/order.go
+++ warmindo-api/db/order.go
@@ -3,7 +3,7 @@ package db
 type Order struct {
 	ID          int    `json:"id"`
 	OrderCode   string `json:"order_code"`
-	Amount      string `json:"amount"`
+	Amount      int    `json:"amount"`
 	TableNumber string `json:"table_number"`
 	StatusID    int    `json:"status_id"`
 	OrderDate   string `json:"order_date"`
